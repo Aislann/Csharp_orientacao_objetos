7b6b3bb44d0277cee2e8b8913a523806ad4a9fba
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-02-27 20:31:11 -0300

    Add files via upload

---
+++ Desafio1/Elevador.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientação_Objetos_C_
+{
+    class Elevador
+    {
+        public int andarAtual {  get; set; }
+        public int totalAndares { get; set; }
+        public int capacidade { get; set; }
+        public int qtdPessoas { get; set; }
+
+        public Elevador(int andarAtual, int totalAndares, int capacidade, int qtdPessoas)
+        {
+            this.andarAtual = andarAtual;
+            this.totalAndares = totalAndares;
+            this.capacidade = capacidade;
+            this.qtdPessoas = qtdPessoas;
+        }
+
+        public void Inicializar()
+        {
+            Console.WriteLine("Digite a capacidade do elevador: " );
+            int opcao1 = Convert.ToInt32(Console.ReadLine());
+            capacidade += opcao1;
+            Console.WriteLine("Digite o total de andares do prédio: ");
+            int opcao2 = Convert.ToInt32(Console.ReadLine());
+            totalAndares += opcao2;
+
+            Console.WriteLine($"Voce está no {andarAtual}º andar \n" +
+                $"Total de andares: {totalAndares}\n" +
+                $"Capacidade do elevador: {capacidade}\n" +
+                $"Quantidade de pessoas presentes no elevador: {qtdPessoas}");
+        }
+
+        public void entrar()
+        {
+            if (qtdPessoas < capacidade) 
+            {
+                qtdPessoas++;
+                Console.WriteLine("A pessoa entrou no elevador ");
+            }
+            else
+            {
+                Console.WriteLine("O elevador está cheio");
+            }
+        }
+
+        public void sair()
+        {
+            if (qtdPessoas > 0)
+            {
+                qtdPessoas--;
+                Console.WriteLine("A pessoa saiu do elevador ");
+            }
+            else
+            {
+                Console.WriteLine("O elevador está vazio");
+            }
+        }
+
+        public void subir()
+        {
+            if (andarAtual == totalAndares)
+            {
+                Console.WriteLine("O elevador está no andar máximo!");
+
+            } else
+            {
+                andarAtual++;
+                Console.WriteLine($"O elevador subiu um andar e está no:{andarAtual}º andar");
+            }
+        }
+
+        public void descer()
+        {
+            if (andarAtual == 0)
+            {
+                Console.WriteLine("O elevado está no terréo");
+
+            }
+            else
+            {
+                andarAtual--;
+                if (andarAtual == 0)
+                {
+                    Console.WriteLine($"O elevador desceu um andar e está no terréo");
+                }
+                else 
+                {
+                    Console.WriteLine($"O elevador desceu um andar e está no:{andarAtual}º andar");
+                }
+                
+            }
+        }
+    }
+
+}
---
+++ Desafio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Orientação_Objetos_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o andar atual: ");
+        int andar = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de pessoas presentes no elevador: ");
+        int quantidade = Convert.ToInt32(Console.ReadLine());
+
+
+        // Exemplo de uso
+        Elevador elevador = new Elevador(andar, 0, 0, quantidade);
+
+        elevador.Inicializar();
+
+        int i = 0;
+        while (i < 1)
+        {
+            Console.WriteLine("\n" +
+                "1- Entrar\n" +
+                "2- Sair\n" +
+                "3- Subir\n" +
+                "4- Descer\n" +
+                "Escolha o que deseja fazer: ");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            if (opcao == 1)
+            {
+                elevador.entrar();
+            } else if (opcao == 2)
+            {
+                elevador.sair();
+                i++;
+                break;
+            } else if(opcao == 3)
+            {
+                elevador.subir();   
+            } else if (opcao == 4)
+            {
+                elevador.descer();
+            }
+            
+        }
+    }
+}
